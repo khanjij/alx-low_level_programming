e7006632bb18be89909e64bba04c961894b63e73
kouar jihane <kouarjihane@gmail.com>
2023-10-06 13:41:31 -0700

    ghjk

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing program C function
+ *write(int fd.const void *buf.size_t count)
+ * Return: Always 1 (error)
+*/
+int main(void)
+{
+	char jih[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, jih, 59);
+	return (1);
+}
