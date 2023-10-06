87cd5eb0852e14601b88639d0283b252b5930b67
kouar jihane <kouarjihane@gmail.com>
2023-10-06 12:13:33 -0700

    ghjkl

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description : puts program C function
+ * Return: always : 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
