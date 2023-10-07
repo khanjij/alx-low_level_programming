f7c9c8890aaab9cfe39d923c0a0f794d15fc456e
kouar jihane <kouarjihane@gmail.com>
2023-10-07 12:03:06 -0700

    hjkl

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: alphabet ascii format
+ *
+ * Return:0 (Success)
+*/
+
+int main(void)
+{
+	int l = 0;
+
+	while (l < 10)
+{
+		printf("%i", l);
+		l++;
+}
+	printf("\n");
+	return (0);
+}
