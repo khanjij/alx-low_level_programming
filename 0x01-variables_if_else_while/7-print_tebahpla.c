b29fbd350e143a1508307be028ce5d2cf2a3f1e5
kouar jihane <kouarjihane@gmail.com>
2023-10-07 12:31:14 -0700

    ghjk

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,22 @@
-az
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: alphabet ascii format
+ *
+ * Return:0 (Success)
+*/
+
+int main(void)
+{
+	char l = 'z';
+
+	while (l >= 'a')
+{
+	putchar(l);
+	l--;
+}
+	putchar('\n');
+	return (0);
+}
