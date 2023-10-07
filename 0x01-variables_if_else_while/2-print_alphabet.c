5395b1fa93d96b1a0ec4a2d029b9a0b9d9990834
kouar jihane <kouarjihane@gmail.com>
2023-10-07 10:28:33 -0700

    yuiop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: alphabet ascii format
+ *
+ * Return:0 (Success)
+*/
+int main(void)
+{
+	int F = 0
+	int D = 97
+
+	while (F <= 27)
+{
+		putchar(F + D);
+		F++;
+}
+	return (0);
+}
