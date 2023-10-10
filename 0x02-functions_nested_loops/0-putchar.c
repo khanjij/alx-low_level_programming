7c8a3c5969a681e1a0e6a9b1d1ae670359ab4144
kouar jihane <kouarjihane@gmail.com>
2023-10-10 10:47:52 -0700

    45

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: program to print putchar
+ *
+ * Returen: 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+}
+	return (0);
