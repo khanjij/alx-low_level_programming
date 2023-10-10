a304ee92117e7db2bd2bc83965eba35e2a221749
kouar jihane <kouarjihane@gmail.com>
2023-10-10 15:11:51 -0700

    tyui

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - utilizes on the _putchar function to print the alphabet 
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
