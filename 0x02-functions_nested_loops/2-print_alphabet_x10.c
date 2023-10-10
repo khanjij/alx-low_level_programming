a5b30f92e57a111c71ea0011b9b87c728bd089ee
kouar jihane <kouarjihane@gmail.com>
2023-10-10 15:32:39 -0700

    hjk

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - fuction will print alphabet lower case 10 timess
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, ch;
+
+	for (i = 0; i < 10; i++)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
+}
