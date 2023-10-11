ec5e9078596e16ae01bd3442db6c0bbd4057fb3b
kouar jihane <kouarjihane@gmail.com>
2023-10-11 11:26:35 -0700

    ghjk

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * Description: fuction will print alphabet lower case 10 timess
+ * print_alphabet_x10 - fuction will print alphabet 10 timess
  *
 */
 
@@ -10,11 +10,11 @@ void print_alphabet_x10(void)
 	int i, ch;
 
 	for (i = 0; i < 10; i++)
-{
+	{
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{	_putchar(ch);
-		_putchar('\n');
-
-	}
+	{
+	_putchar(ch);
+}
+	_putchar('\n');
 }
 }
