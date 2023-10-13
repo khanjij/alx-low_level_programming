729dc63c04aba46a16808c827f55dfa61765369c
kouar jihane <kouarjihane@gmail.com>
2023-10-13 16:02:29 -0700

    hjk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * *_abs - function that computes the absolute
+ *	value of an integer
+ *
+ * @n: takes ieteger type input function
+ *
+ * Return: Always 0 (success)
+*/
+
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+else
+return (n);
+}
