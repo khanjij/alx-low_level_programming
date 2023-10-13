27259aa6cd787cf8e1894ffdfafbae5fb850c70e
kouar jihane <kouarjihane@gmail.com>
2023-10-13 16:30:30 -0700

    hjk

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -16,6 +16,6 @@ int print_last_digit(int n)
 		lastdigit = -1 * (n % 10);
 	else
 		lastdigit = n % 10;
-	r = _putchar(lastdigit + '0');
-	return (r);
+	_putchar(lastdigit + '0');
+	return (lastdigit);
 }
