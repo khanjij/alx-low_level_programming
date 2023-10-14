3a12d151112dd474048e120661ad5c96065d44e5
kouar jihane <kouarjihane@gmail.com>
2023-10-13 17:41:51 -0700

    yuio

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,10 +11,14 @@ void print_to_98(int n)
 {
 int m
 	if (n > 98)
+	{
 		for (m = n; m > 98 ; m--)
 			printf("%d", m);
+	}
 	else
+	{
 		for (m = n; m < 98 ; m++)
 			printf("%d", m);
+	}
 	printf("98\n");
 }
