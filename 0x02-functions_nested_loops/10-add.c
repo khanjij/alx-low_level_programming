c5d3e9ea1aa0d113ad1555078104e5a96c7c520f
kouar jihane <kouarjihane@gmail.com>
2023-10-13 17:17:15 -0700

    hjk

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * add - add number integer
+ *
+ * @i: first integer
+ * @j: seconde integer
+ *
+ * Return: sum of j and i
+ */
+
+int add(int i, int j)
+{
+	int i, j, s;
+
+	s = i + j;
+	return (s);
+}
