e9d47d01a5ae7d677e5a7c15447e0b4df055cf64
kouar jihane <kouarjihane@gmail.com>
2023-10-14 12:53:57 -0700

    ghjk

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _isdigit - check for digit (0 to 9)
+ *
+ * @c: input function
+ *
+ * Return: 1 if digit,else 0
+ */
+
+int _isdigit(int c)
+{
+	if (c <= 48 && c >= 57)
+		return (1);
+	else
+		return (0);
+}
