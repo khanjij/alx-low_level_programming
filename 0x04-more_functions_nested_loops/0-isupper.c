bfb34f3851a803ab95b8024e1353cd3095029d2f
kouar jihane <kouarjihane@gmail.com>
2023-10-14 12:01:33 -0700

    ghjk

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include"mainh"
+
+/**
+ * _isupper - define if character uppercase
+ *
+ * @c: character input function
+ *
+ * Return: 1 if c uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
