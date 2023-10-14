96e0cebc352234d36b495baf3648e5c3372d0ce2
kouar jihane <kouarjihane@gmail.com>
2023-10-14 14:13:45 -0700

    hjk

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * more_numbers - print number from 0 to 14 ten times
+ *
+ * return: result
+ */
+
+void more_numbers(void)
+{
+	int i, n;
+
+	for (i = 0; i <= 9 ; i++)
+	{
+		for (n = 0; n <= 14 ; n++)
+{
+			if (n > 9)
+				{
+				_putchar((n / 10) + 48);
+				}
+			_putchar((n % 10) + 48);
+		}
+	}
+	_putchar('\n');
+}
