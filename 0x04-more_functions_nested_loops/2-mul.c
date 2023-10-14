8173d20236931dce1873aeba3881c698f364417f
kouar jihane <kouarjihane@gmail.com>
2023-10-14 13:06:55 -0700

    hjk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * mult(int a, int b) - print mult of number
+ *
+ * @a: input function
+ * @b: input function
+ * Return: return multiple
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
