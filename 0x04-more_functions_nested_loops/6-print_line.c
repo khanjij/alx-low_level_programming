424114cf1d8dce2b1125475cbe03b5fd92644ac8
kouar jihane <kouarjihane@gmail.com>
2023-10-14 14:30:42 -0700

    hjkl

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * print_line - print character n time
+ *
+ * @n: input function
+ *
+ * Return: result 0
+ */
+
+int print_line(int n)
+{
+	int c;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (c = 1; c <= n ; c++)
+		{
+		_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
