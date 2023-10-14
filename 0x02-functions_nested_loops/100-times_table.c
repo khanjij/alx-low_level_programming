f7211b40989316bfd56e4afb2efce90852d34e38
kouar jihane <kouarjihane@gmail.com>
2023-10-13 18:27:03 -0700

    hjk

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include"main.h"
+
+/**
+ * print_times_table - prints table times of number integer.
+ *
+ * @n: input integer function.
+ */
+
+void print_times_table(int n)
+{
+	int p, i, j;
+
+	if (n <= 15 && n >= 0)
+{
+	for (i = 0; i <= n ; i++)
+{
+	_putchar(48);
+	for (j = 1; j <= n ; j++)
+	{
+	p = i * j;
+		_putchar(',');
+		_putchar(' ');
+		if (p <= 9)
+		{
+			_putchar(' ');
+		}
+		if (p <= 99)
+		{
+			_putchar(' ');
+		}
+		if (p >= 100)
+		{
+			_putchar((p / 100) + 48);
+			_putchar((p % 100) + 48);
+		}
+		else if (p <= 99 && p >= 10)
+		{
+		_putchar((p / 10) + 48);
+		_putchar((p % 10) + 48);
+		}
+	_putchar('\n');
+	}
+}
+}
+}
