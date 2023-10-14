ba62c33804cc250569c39a9898efb5f79954ba52
kouar jihane <kouarjihane@gmail.com>
2023-10-13 17:06:13 -0700

    hjk

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,17 +12,21 @@ void times_table(void)
 	for (i = 0; i <= 9 ;i++)
 {
 	_putchar(48);
-		for (j = 1; j <= 9, j++)
+		for (j = 1; j <= 9; j++)
 		{
 			m = i * j;
 	_putchar(',');
 	_putchar(' ');
 if (m <= 9)
+{
 	_putchar(' ');
+}
 else
+{
 	_putchar((m / 10) + 48);
 	_putchar((m % 10) + 48);
-		}
+}		
+}
 	_putchar('\n');
 }
 }
