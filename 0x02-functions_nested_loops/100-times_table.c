55b801d6ad40615e2f617140569e39f99ea9099a
kouar jihane <kouarjihane@gmail.com>
2023-10-14 19:05:50 -0700

    hjk

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 {
 	int p, i, j;
 
-	if (n <= 15 && n >= 0)
+	if (n >= 0 && n <= 15)
 {
 	for (i = 0; i <= n ; i++)
 {
@@ -26,7 +26,7 @@ void print_times_table(int n)
 			_putchar(' ');
 			_putchar(p + 48);
 		}
-		else if (p <= 99 && p >= 10)
+		else if (p <= 99)
 		{
 			_putchar(' ');
 			_putchar((p / 10) + 48);
@@ -35,8 +35,8 @@ void print_times_table(int n)
 			else if (p >= 100)
 			{
 			_putchar((p / 100) + 48);
-			_putchar(((p % 10) / 10) + 48);
-			_putchar(((p % 10) % 10) + 48);
+			_putchar(((p / 10) % 10) + 48);
+			_putchar((p % 10) + 48);
 			}
 	}
 	_putchar('\n');
