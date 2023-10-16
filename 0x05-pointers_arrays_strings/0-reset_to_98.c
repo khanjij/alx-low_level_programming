f12f420b7cced5464092dc24dc68932cd1eea4e3
kouar jihane <kouarjihane@gmail.com>
2023-10-16 13:44:31 -0700

    yui

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * rest_to_98 - take pointer to int value to 98
+ *
+ * @n: int parameter
+ *
+ * Return: result
+ */
+
+void rest_to_98(int *n)
+{
+	int *n;
+	int n;
+
+	*n = 98;
+}
