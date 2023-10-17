10230bcbe2c1fbc6bc0c1b77e2a6f3ae793368e2
kouar jihane <kouarjihane@gmail.com>
2023-10-16 17:37:29 -0700

    jihane

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,21 @@
 #include"main.h"
 
 /**
+ * puts2 - print every other character
  *
+ * @str: string character
  *
- *
- *
- *
+ *Return: result
  */
 
+void puts2(char *str);
+{
+	int i;
+
+for (i = 0; str[i] != '\0' ; ++i)
+{
+	if (i % 2 == 0)
+	_putchar(srt[i]);
+}
+_putchar('\n');
+}
