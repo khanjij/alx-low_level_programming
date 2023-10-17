1d0be596c040f5461e2c4b957efaab415ac29e65
kouar jihane <kouarjihane@gmail.com>
2023-10-17 06:01:14 -0700

    jihane

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@
 
 void rev_string(char *s)
 {
-int l ;
+int l;
 int i;
 char temp;
 
@@ -19,8 +19,8 @@ for (l = 0; s[l] != '\0' ; ++l)
 	for (i = 0; i < l / 2 ; i++)
 	{
 	temp = s[i];
-	s[i] = s[l -1 - i];
-	s[l -1 - i] = temp;
+	s[i] = s[l - 1 - i];
+	s[l - 1 - i] = temp;
 	}
 }
 }
