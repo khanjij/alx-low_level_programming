c59f065b798800e1bef2055accdf927c887db42e
kouar jihane <kouarjihane@gmail.com>
2023-10-16 18:40:32 -0700

    jihane

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,23 @@
-#i
+#include"main.h"
+/**
+ * print_array - print element array
+ *
+ * @n: number element
+ * @a: character string
+ *
+ * Return: result
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	_putchar('\n');
+}
