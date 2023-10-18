a97f4f7d0eafc1eff5ec29efbcd101bedffc3d05
kouar jihane <kouarjihane@gmail.com>
2023-10-18 16:42:06 -0700

    jihane

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _strcmp - function to coppare two string character
+ *
+ * @s1: character string1
+ * @s2: character string2
+ *
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2);
+{
+	int cmp = 0;
+
+for (; *s1 != '\0' ; s1++)
+{
+for (; *s2 != '\0' ; s2++)
+{
+/*	while (*s1 != '\0' && *s2 != '\0') */
+/*	if (*s1 != *s2) */
+
+	cmp = *s1 - *s2;
+/* s1++; */
+/* s2++; */
+}
+}
+return (cmp);
+}
