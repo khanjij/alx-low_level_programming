bd80680740ab045b9a53fb44f18104daf394b8dd
kouar jihane <kouarjihane@gmail.com>
2023-10-18 16:46:07 -0700

    jihane

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,17 +14,15 @@ int _strcmp(char *s1, char *s2);
 {
 	int cmp = 0;
 
-for (; *s1 != '\0' ; s1++)
+while (*s1 != '\0' && *s2 != '\0')
 {
-for (; *s2 != '\0' ; s2++)
-{
-/*	while (*s1 != '\0' && *s2 != '\0') */
-/*	if (*s1 != *s2) */
-
-	cmp = *s1 - *s2;
-/* s1++; */
-/* s2++; */
-}
+	if (*s1 != *s2)
+	{
+		cmp = *s1 - *s2;
+		break;
+	}
+	s1++;
+	s2++;
 }
 return (cmp);
 }
