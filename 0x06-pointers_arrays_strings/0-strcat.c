71ce91b9592ef1c364a2b59a3be8ba7865fdfcc9
kouar jihane <kouarjihane@gmail.com>
2023-10-18 14:44:04 -0700

    jihane

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ *_strcat - concatenate two string character
+ *
+ * @src: string of character
+ * @dest: string of character
+ *
+ * Return: string character
+ */
+char *_strcat(char *dest, char *src)
+{
+	int c, c2;
+
+c = 0;
+while (dest[c] != '\0')
+{
+c++;
+c2 = 0;
+}
+while (src[c2] != '\0')
+{
+dest[c] = src[c2];
+c++;
+c2++;
+}
+return (dest);
+}
