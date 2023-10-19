22d1399bdeae2643783189f5f87d328c1e0fee61
kouar jihane <kouarjihane@gmail.com>
2023-10-18 17:05:10 -0700

    jihane

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * reverse_array - function reverse array of integer
+ *
+ * @a: array of integer
+ * @n: number of integer
+ *
+ * Return: result
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+
+for (i = 0; i < j; i++)
+{
+for (j = n - 1; j > i; j--)
+	{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+}
+}
+}
