04fef314f5acb48f454a42a8a5659c6f4c2c5ecf
kouar jihane <kouarjihane@gmail.com>
2023-10-19 13:04:27 -0700

    jihane

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,6 +10,8 @@
  */
 char *string_toupper(char *str)
 {
+	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
@@ -17,4 +19,5 @@ char *string_toupper(char *str)
 			str[i] = str[i] + 32;
 		}
 	}
+return (str);
 }
