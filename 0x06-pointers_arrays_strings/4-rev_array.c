0c4d40e76361efc8c46bc54f845de1e78ab07d62
kouar jihane <kouarjihane@gmail.com>
2023-10-19 12:51:27 -0700

    jihane

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,13 +14,10 @@ void reverse_array(int *a, int n)
 {
 	int temp, i, j;
 
-for (i = 0; i < j; i++)
+for (i = 0, j = (n-1); i < j; i++, j--)
 {
-for (j = n - 1; j > i; j--)
-	{
 temp = a[i];
 a[i] = a[j];
 a[j] = temp;
-}
 }
 }
