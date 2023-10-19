3962fd0ad9461280eb549babeae762c489880e6f
kouar jihane <kouarjihane@gmail.com>
2023-10-19 13:03:13 -0700

    jihane

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ *string_toupper - function replace lowercase by uppercase
+ *
+ * @str: string to return
+ *
+ *Return: string character
+ */
+char *string_toupper(char *str)
+{
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] + 32;
+		}
+	}
+}
