16d6188e07ec1d1ba8275a8e1749e056a59df9f3
kouar jihane <kouarjihane@gmail.com>
2023-10-20 15:46:09 -0700

    jihane

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,55 +2,29 @@
 #include "main.h"
 
 /**
- * isLower - determine whether ascii is lower
- * @c: character
- * Return: 1 if true, 0 if false
- */
-int isLower(char c)
-{
-return (c >= 'a' && c <= 'z');
-}
-
-/**
- * isDelimiter - determine whether ascii is delimiter
- * @c: character
- * Return: 1 if true, 0 if false
- */
-int isDelimiter(char c)
-{
-char delimiter[] = " \t\n,;.!?\"(){} ";
-
-for (int i = 0; i < 13; i++)
-{
-if (c == delimiter[i])
-return (1);
-}
-return (0);
-}
-
-/**
  * cap_string - capitalize all words of a string
  * @str: input string
  * Return: string with capitalized words
  */
 char *cap_string(char *str)
 {
-char *ptr = str;
-int foundDelimiter = 1;
+int i = 0;
 
-while (*str != '\0')
+while (str[i])
 {
-if (isDelimiter(*str))
-foundDelimiter = 1;
-else if (isLower(*str) && foundDelimiter)
+while (!(str[i] >= 'a' && str[i] <= 'z'))
 {
-*str = *str - ('a' - 'A');
-foundDelimiter = 0;
-}
-else
-foundDelimiter = 0;
+i++;
 
-str++;
+if ((str[i - 1] == ' ') || (str[i - 1] == ',') || (str[i - 1] == ';') ||
+(str[i - 1] == '!') || (str[i - 1] == '"') || (str[i - 1] == '(') ||
+(str[i - 1] == ')') || (str[i - 1] == '{') || (str[i - 1] == '}') ||
+(str[i - 1] == '?') || (str[i - 1] == '.') ||
+(str[i - 1] == '\t') || (str[i - 1] == '\n') || i == 0)
+{
+str[i] = str[i] - 32;
+i++;
 }
-return (ptr);
+}
+return (str);
 }
