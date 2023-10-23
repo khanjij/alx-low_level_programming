26648af1139be77daca7e490bdc0ba167fca036c
kouar jihane <kouarjihane@gmail.com>
2023-10-23 13:46:19 -0700

    firstocu

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,18 +4,20 @@
  * _strchr - function locates a character in  a string
  * @c: character to search
  * @s: pointer string caracter
- * return: pointer to 1st occurence true or null false
+ * Return: pointer to 1st occurence true or null false
  */
 
 char *_strchr(char *s, char c)
 {
 	int i;
+	char *p = &s[0];
 
 for (i = 0; ; i++)
 {
 	if (s[i] == c)
 	{
-	return (s + i);
+	p = p + i;
+		return (p);
 	}
 }
 	return ('\0');
