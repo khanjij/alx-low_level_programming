3cd22161f07f3c6e3d3c441d871b0f50c992f5e8
kouar jihane <kouarjihane@gmail.com>
2023-10-23 16:52:16 -0700

    jih

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _strpbrk - locate first occurence of c in string
+ * @s: string character
+ * @accept: bytes to search
+ * Return: pointer to byte s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	while(s[j]= 0 )
+		for (i = 0 ; accept[i] != '\0' ; j++)
+			if (accept[i] == s[i])
+			return (s[i])
