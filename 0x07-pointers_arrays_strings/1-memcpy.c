e982d7b84ac0d49416a0301444f2f3ccb093f7e2
kouar jihane <kouarjihane@gmail.com>
2023-10-23 11:23:55 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-while (i < n)
-{
-	*dest = *src;
-	dest++;
-	src++;
-}
-return (dest);
+	for (i = 0; i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
