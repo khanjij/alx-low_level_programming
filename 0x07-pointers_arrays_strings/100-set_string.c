97023ad690e1157221f2fff82bfeae59349ce8fd
kouar jihane <kouarjihane@gmail.com>
2023-10-24 18:59:07 -0700

    crack101

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -8,34 +8,5 @@
  */
 void set_string(char **s, char *to)
 {
-	int l1 = 0;
-	int l2 = 0;
-	int i, j;
-
-	while (**s != '\0')
-		l1++;
-	while (*to != '\0')
-		l2++;
-	l2 = l2 - 1;
-	l1 = l1 - 1;
-	if (l2 < l1)
-	{
-		for (j = 0 ; j <= l2 ; j++)
-		{
-			**s = to[j];
-			s++;
-		}
-		while (j <= l1)
-			**s = '\0';
-		j++;
-		s++;
+	*s = to;
 	}
-	if (l2 >= l1)
-	{
-		for (i = 0 ; i <= l2 ; i++)
-		{
-			**s = to[i];
-			s++;
-		}
-	}
-}
