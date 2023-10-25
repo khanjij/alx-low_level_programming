b130b527fd086a0759183f1a9161ce332cedd4ad
kouar jihane <kouarjihane@gmail.com>
2023-10-25 09:16:39 -0700

    putsrecursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _puts_recursion - print a string
+ * @s: string char
+ * Return: string character
+ */
+void _puts_recursion(char *s)
+{
+_putchar(*s);
+s++;
+	if (*s != '\0')
+	{
+	_puts_recursion(s);
+	}
+}
