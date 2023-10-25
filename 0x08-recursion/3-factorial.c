a72ab34daa8e50bd95fc3269a4ea50d39a8a5331
kouar jihane <kouarjihane@gmail.com>
2023-10-25 15:40:12 -0700

    uio

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * factorial - the factorial of n
+ * @n: number input
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+
+	if (n == 0)
+	return (1);
+
+	if (n > 0)
+	return (n * factorial( n - 1));
+
+	return (0);
+}
