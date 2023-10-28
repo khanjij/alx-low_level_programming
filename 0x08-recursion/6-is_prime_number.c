f71d46dea9b252a195d69b4b26a456af5602c24d
kouar jihane <kouarjihane@gmail.com>
2023-10-28 16:25:57 -0700

    k

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,14 +7,14 @@
 int is_prime_number(int n)
 {
 	if (n <= 1)
-	return (0);
-	if (n <= 3)
-	return (1);
-for (int i = 2; i * i <= n; i++)
-{
-	if (n % 2 == 0)
 		return (0);
-		else
+	if (n <= 3)
 		return (1);
-}
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+			return (0);
+		else
+			return (1);
+	}
 }
