09274b018413360ddc68ca65a101407e513c29c8
kouar jihane <kouarjihane@gmail.com>
2023-10-28 17:24:14 -0700

    k

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,32 @@
 #include"main.h"
 /**
- *
- *
- *
- *
+ * _sqrt_recursion - function that returns square of num
+ * @n: the input number
+ * Return: return square otherwise -1
  */
 int _sqrt_recursion(int n)
 {
-	if 
+if (n < 0)
+	return (-1);
+return (the_sqrt(n, 1));
+}
+
+/**
+ * the_sqrt - function calculate square
+ * @n: number
+ * @i: square
+ * Return: result
+ */
+int the_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+
+	if (i * i == 0)
+		return (i);
+
+if (i * i < n)
+return (the_sqrt(n, i + 1));
+
+return (0);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -19,5 +19,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int prime_num(int n, int i);
+int the_sqrt(int n, int i);
 #endif
 
