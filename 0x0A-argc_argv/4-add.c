95df820f5d09f54201b53f65d4de62341e88591c
kouar jihane <kouarjihane@gmail.com>
2023-11-04 14:07:04 -0700

    uio

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,29 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+/**
+ * main - add positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no number ,newline
+ *	1 if symbol Error , NL
+ *	integer adds if numbers
+ */
 
 int main(int argc, char *argv[])
 {
+	int num;
+	int sum = 0;
+
 	if (argc == 1)
 	{
 		printf("0\n");
-		return 0;
 	}
 
-	int sum = 0;
 
+	if (argc > 1)
+{
 	for (int i = 1; i < argc; i++)
 	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
+		/*for (int j = 0; argv[i][j] != '\0'; j++)*/
 		{
-			if (!isdigit(argv[i][j]))
+			if (!isdigit(argv[i]))
 			{
 				printf("Error\n");
-				return 1;
+				return (1);
+			break;
 			}
 		}
 
-		int num = atoi(argv[i]);
+		num = atoi(argv[i]);
 		sum += num;
 	}
 
