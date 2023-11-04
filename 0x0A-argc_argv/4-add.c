21b2c3d0b2645ef103c3443117740fba177abd73
kouar jihane <kouarjihane@gmail.com>
2023-11-04 14:31:05 -0700

    uio

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,7 @@
-#include"main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
 /**
  * main - add positive numbers
  * @argc: argument count
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-	for (j = 0; argv[i][j] != '\0'; j++)
+	for (j = 0; argv[i][j] != '\0' ; j++)
 	{
 			if (!isdigit(argv[i][j]))
 			{
@@ -41,4 +41,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
-
