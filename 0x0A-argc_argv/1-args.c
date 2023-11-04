06c085b2b6fce18a702d597ff7ca5217e0f43033
kouar jihane <kouarjihane@gmail.com>
2023-11-04 13:29:28 -0700

    uio

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,5 @@
 #include"main.h"
-#include<stdio.h>
+i#include<stdio.h>
 /**
  * main - print the number of arguments passed to program
  * @argc: number of argument
@@ -9,7 +9,7 @@
 
 int main(int argc, char *argv[])
 {
-printf("%d\n", argc - 1);
+printf("%d\n", argc);
 
 return (0);
 }
