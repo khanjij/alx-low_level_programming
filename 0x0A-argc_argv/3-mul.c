9a6ee9a09788b24c808640959e0bfa2c81e71666
kouar jihane <kouarjihane@gmail.com>
2023-11-04 13:52:27 -0700

    uio

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,19 +1,26 @@
-#include"main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - multiple of two number
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: integer multiple otherwise 1
+ */
 
 int main(int argc, char *argv[])
 {
+	int r, n1, n2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
 
-	int r = num1 * num2;
+	r = n1 * n2;
 
 	printf("%d\n", r);
 
