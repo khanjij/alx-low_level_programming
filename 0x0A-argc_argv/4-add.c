1d43d4a7ffafb263ad968f63dba4f2931a16a0d5
kouar jihane <kouarjihane@gmail.com>
2023-11-04 14:11:04 -0700

    uio

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,9 +23,9 @@ int main(int argc, char *argv[])
 
 	for (int i = 1; i < argc; i++)
 	{
-		/*for (int j = 0; argv[i][j] != '\0'; j++)*/
+	for (int j = 0; argv[i][j] != '\0'; j++)
 
-			if (!isdigit(argv[i]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
