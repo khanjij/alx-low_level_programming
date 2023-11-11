a6cf9e189f67e651761f259806ba4efe7b1bb828
kouar jihane <kouarjihane@gmail.com>
2023-11-11 04:57:34 -0800

    varadic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include<stdarg.h>
+#include<stdio.h>
 /**
  * print_numbers - prints numbers given as parameters
  * @separator: string to be printed between numbers
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include"variadic_function.h"
+#include<stdarg.h>
+#include<stdio.h>
+/**
+ * print_strings - func to print character with separator
+ * @separator: separator between string
+ * @n: number of argument to print
+ * Return: args
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args,n);
+
+	for (i = 0; i < n; i++)
+	{
+		char *str = va_arg(args, char *);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
